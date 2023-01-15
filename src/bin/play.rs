457d548c4f8ff2170ca1f12003c77f9ae9c3a0fb
ChaosNyaruko <cabbageparadise@gmail.com>
2023-01-15 15:07:12 +0800

    iter_mut

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -4,5 +4,12 @@ fn main() {
     let r1 = &x;
     let r2 = &r1;
     let r3 = &&x;
-    println!("{:}, {:p}, {:p}, {:p}",x, r1, r2, r3); 
+    println!("{:}, {:p}, {:p}, {:p}", x, r1, r2, r3);
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    println!("{:?}", v);
+    for x in v.iter_mut() {
+        *x += 1;
+    }
+    println!("{:?}", v);
 }
